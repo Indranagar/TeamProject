5287ec4f98dc0658080027a6b3448ee4ed9d7271
bhanu chandra <bhanuchandra2010@live.com>
2017-01-14 19:00:36 +0530

    replaced

--- Flight_Reservation_System/Flight_Reservation_System/Properties/AssemblyInfo.cs
+++ Flight_Reservation_System/Flight_Reservation_System/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebApplication1")]
+[assembly: AssemblyTitle("Flight_Reservation_System")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApplication1")]
+[assembly: AssemblyProduct("Flight_Reservation_System")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4c357e5a-5342-417e-bbf8-780ca5646843")]
+[assembly: Guid("18fcd601-4948-4860-bb4b-b6159ffdcc96")]
 
 // Version information for an assembly consists of the following four values:
 //
