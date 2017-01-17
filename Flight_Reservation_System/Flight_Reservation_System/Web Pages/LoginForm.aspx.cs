2b2b019875714523b858d02d8e27ba7074e48ff7
Dablesh Sharma <dubsy14@gmail.com>
2017-01-17 23:17:51 +0530

    Update

--- Flight_Reservation_System/Flight_Reservation_System/Web Pages/LoginForm.aspx.cs
+++ Flight_Reservation_System/Flight_Reservation_System/Web Pages/LoginForm.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Flight_Reservation_System
 {
-    public partial class Login : System.Web.UI.Page
+    public partial class LoginForm : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
